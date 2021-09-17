cff79cf8c3b62ae700ea3fe31b1aa76ab4699717
arnab.chowdhury <arnab.chowdhury@dreambroker.fi>
2021-09-17 16:06:16 +0300

    Initial commit
    Created the project with spring initializr

---
+++ src/main/java/udemy/springframework/SpringFrameworkUdemyPetClinic/SpringFrameworkUdemyPetClinicApplication.java
@@ -0,0 +1,13 @@
+package udemy.springframework.SpringFrameworkUdemyPetClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkUdemyPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkUdemyPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/udemy/springframework/SpringFrameworkUdemyPetClinic/SpringFrameworkUdemyPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package udemy.springframework.SpringFrameworkUdemyPetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFrameworkUdemyPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
